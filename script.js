9dcc641670002f69d0f5086d25a5ea31b8d0ae02
Dabbett <abbettdylan@gmail.com>
2023-12-08 13:16:58 -0500

    add 2 pages

--- script.js
+++ script.js
@@ -13,3 +13,21 @@ if (close) {
         nav.classList.remove('active')
     })
 }
+
+function incrementQuantity() {
+    document.getElementById('quantity1').stepUp();
+}
+  
+ function decrementQuantity() {
+    document.getElementById('quantity1').stepDown();
+ }
+  
+function updateSubtotal() {
+    var quantity = document.getElementById('quantity').value;
+    var price = document.getElementById('price1').value;
+    var subtotal = `$${parseFloat(quantity)} * ${parseFloat(price)}`;
+    document.getElementById('subtotal').innerText = '$' + subtotal.toFixed(2);
+    }
+
+document.getElementById('quantity1').addEventListener('input', updateSubtotal);
+ 
